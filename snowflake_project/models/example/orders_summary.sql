3c6792222d8a3ce9b73706922be04bb1611f5cff
Raghav <raghav.v@jmangroup.com>
2026-08-29 10:14:22 +0530

    Delete snowflake_project directory

--- snowflake_project/models/example/orders_summary.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='table') }}
-SELECT
-    customer_id,
-    COUNT(*) AS order_count,
-    SUM(amount) AS total_spent
-FROM {{ source('dcm_raw', 'ORDERS_RAW') }}
-GROUP BY customer_id
